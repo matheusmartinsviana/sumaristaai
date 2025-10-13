fba63fd8dbae93bee492667b30b435daa72f47bd
Viana <matheusmviana@outlook.com.br>
2025-10-13 11:31:16 -0300

    feat: add sitemap and robots.txt configuration for SEO optimization

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://sumaristaai.vercel.app',
+  generateRobotsTxt: true, // gera robots.txt automaticamente
+  changefreq: 'daily',
+  priority: 0.7,
+  sitemapSize: 5000,
+};
